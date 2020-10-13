fa07859b97e6d8a298b86cba144999cad2f97a14
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-10-13 15:54:30 +0200

    fix

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -2,6 +2,8 @@ Sys.setenv(RECORDING = FALSE)
 
 if (Sys.getenv("RECORDING")) {
   with_mock_api <- httptest::capture_requests
+} else {
+  Sys.setenv("GITHUB_PAT" = "foobar")
 }
 
 library(httptest)
