afbdadb69dcb26dcf4b470def1210b2e45d917f4
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-10-15 14:00:05 +0200

    use vcr

--- tests/testthat/test-api-status.R
+++ tests/testthat/test-api-status.R
@@ -1,3 +1,5 @@
-test_that("gh_api_status() works", {
-  testthat::expect_type(gh_api_status(), "character")
+vcr::use_cassette("gh_api_status", {
+  test_that("gh_api_status() works", {
+    testthat::expect_type(gh_api_status(), "character")
+  })
 })
--- tests/testthat/test-organizations.R
+++ tests/testthat/test-organizations.R
@@ -1,3 +1,5 @@
-test_that("gh_organizations works", {
-  testthat::expect_type(gh_organizations(), "character")
+vcr::use_cassette("gh_organizations", {
+  test_that("gh_organizations works", {
+    testthat::expect_type(gh_organizations(), "character")
+  })
 })
