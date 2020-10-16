56cb2ac29d0de11c96e5a0ee3abc5eb126917808
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-10-16 11:34:53 +0200

    add test for an error

--- tests/testthat/test-organizations.R
+++ tests/testthat/test-organizations.R
@@ -3,3 +3,10 @@ test_that("gh_organizations works", {
   testthat::expect_type(gh_organizations(), "character")
 })
 })
+
+with_mock_api({
+  test_that("gh_organizations errors if the API doesn't behave", {
+    # first capture_requests( , simplify = FALSE) then edit
+    testthat::expect_error(gh_organizations())
+  })
+})
