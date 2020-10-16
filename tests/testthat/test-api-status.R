cd6fc03b9de4d4da22f551b12778989c7909270e
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-10-16 15:11:47 +0200

    add 1 test with presser

--- tests/testthat/test-api-status.R
+++ tests/testthat/test-api-status.R
@@ -1,3 +1,27 @@
 test_that("gh_api_status() works", {
+  if(!nzchar(Sys.getenv("REAL_REQUESTS"))) {
+   app <- presser::new_app()
+      app$get("/", function(req, res) {
+        res$send_json(
+          list( components =
+          list(
+            list(
+            name = "API Requests",
+            status = "operational"
+            )
+          )
+          ),
+          auto_unbox = TRUE
+        )
+      })
+    web <-  presser::new_app_process(app)
+    withr::defer(web$stop())
+    mockery::stub(
+      where = gh_api_status,
+      what = "status_url",
+      how = web$url
+    )
+  }
+
   testthat::expect_type(gh_api_status(), "character")
 })
