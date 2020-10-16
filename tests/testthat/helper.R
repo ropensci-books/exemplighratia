64d6e24fdb107288d8b938b399207382fae75a38
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-10-16 11:34:53 +0200

    add test for an error

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,14 +1,19 @@
 with_mock_dir <- function(dir, ...) {
   dir <- testthat::test_path(dir)
+  current_mockPaths <- httptest::.mockPaths()
   httptest::.mockPaths(dir)
   if (dir.exists(dir)) {
     ## We already have recorded, so use the fixtures
-    Sys.setenv("GITHUB_PAT" = "foobar")
-    httptest::with_mock_api(...)
+    withr::with_envvar(
+      new = c("GITHUB_PAT" = "foobar"),
+      httptest::with_mock_api(...)
+    )
   } else {
     ## Record!
     httptest::capture_requests(...)
   }
+  httptest::.mockPaths(NULL)
+  httptest::.mockPaths(current_mockPaths)
 }
 
 library(httptest)
