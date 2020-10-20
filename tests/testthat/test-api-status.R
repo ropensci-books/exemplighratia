b646c9178c761e09cbc1f9896da7520157038155
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-10-20 14:02:31 +0200

    better @sckott? :-)

--- tests/testthat/test-api-status.R
+++ tests/testthat/test-api-status.R
@@ -1,5 +1,6 @@
-vcr::use_cassette("gh_api_status", {
-  test_that("gh_api_status() works", {
-    testthat::expect_type(gh_api_status(), "character")
+test_that("gh_api_status() works", {
+  vcr::use_cassette("gh_api_status", {
+    status <- gh_api_status()
   })
+  testthat::expect_type(status, "character")
 })
