513a284d3ee0b68cc41ad1504d20b968f4f7bbab
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-10-23 10:17:28 +0200

    better

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,7 +1,9 @@
-fake_pat <- function() {
-  withr::local_envvar(
-    new = c("GITHUB_PAT" = "foobar")
-  )
-}
-
 library(httptest)
+
+# for contexts where the package needs to be fooled
+# (CRAN, forks)
+# this is ok because the package will used recorded responses
+# so no need for a real secret
+if (!nzchar(Sys.getenv("GITHUB_PAT"))) {
+  Sys.setenv(GITHUB_PAT = "foobar")
+}
--- tests/testthat/test-organizations.R
+++ tests/testthat/test-organizations.R
@@ -1,16 +1,12 @@
 with_mock_dir("gh_organizations", {
 test_that("gh_organizations works", {
   testthat::expect_type(gh_organizations(), "character")
+  })
 })
-},
-mock_prep = { fake_pat() }
-)
 
 with_mock_dir("gh_organizations_error", {
   test_that("gh_organizations errors if the API doesn't behave", {
     # first capture_requests( , simplify = FALSE) then edit
     testthat::expect_error(gh_organizations())
   })
-},
-mock_prep = { fake_pat() }
-)
+})
