445ea28b1fd780ea8f7665b7ee19e23daa3beba4
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-10-23 10:11:22 +0200

    tweaks

--- tests/testthat/test-organizations.R
+++ tests/testthat/test-organizations.R
@@ -2,11 +2,15 @@ with_mock_dir("gh_organizations", {
 test_that("gh_organizations works", {
   testthat::expect_type(gh_organizations(), "character")
 })
-})
+},
+mock_prep = { fake_pat() }
+)
 
-with_mock_api({
+with_mock_dir("gh_organizations_error", {
   test_that("gh_organizations errors if the API doesn't behave", {
     # first capture_requests( , simplify = FALSE) then edit
     testthat::expect_error(gh_organizations())
   })
-})
+},
+mock_prep = { fake_pat() }
+)
