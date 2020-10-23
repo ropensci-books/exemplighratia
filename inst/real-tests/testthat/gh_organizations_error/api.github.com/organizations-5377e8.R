be69315468635df7e1804243d107c0561a6f9a30
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-10-23 11:34:02 +0200

    oops

--- inst/real-tests/testthat/gh_organizations_error/api.github.com/organizations-5377e8.R
+++
@@ -1,3 +0,0 @@
-structure(list(url = "https://api.github.com/organizations?since=1",
-    status_code = 502L, headers = NULL), class = "response")
-
--- inst/real-tests/testthat/test-api-status.R
+++ inst/real-tests/testthat/test-api-status.R
@@ -1,6 +1,3 @@
-with_mock_dir("gh_api_status", {
 test_that("gh_api_status() works", {
   testthat::expect_type(gh_api_status(), "character")
-  testthat::expect_equal(gh_api_status(), "operational")
-})
 })
