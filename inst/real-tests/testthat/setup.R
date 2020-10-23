2b8264e047261cde33bbd21f68efe82091151ee8
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-10-23 11:34:02 +0200

    oops

--- inst/real-tests/testthat/setup.R
+++
@@ -1,9 +0,0 @@
-library(httptest)
-
-# for contexts where the package needs to be fooled
-# (CRAN, forks)
-# this is ok because the package will used recorded responses
-# so no need for a real secret
-if (!nzchar(Sys.getenv("GITHUB_PAT"))) {
-  Sys.setenv(GITHUB_PAT = "foobar")
-}
--- inst/real-tests/testthat/test-organizations.R
+++ inst/real-tests/testthat/test-organizations.R
@@ -1,13 +1,3 @@
-with_mock_dir("gh_organizations", {
 test_that("gh_organizations works", {
   testthat::expect_type(gh_organizations(), "character")
-  })
 })
-
-with_mock_dir("gh_organizations_error", {
-  test_that("gh_organizations errors if the API doesn't behave", {
-    # first capture_requests( , simplify = FALSE) then edit
-    testthat::expect_error(gh_organizations())
-  })
-},
-simplify = FALSE)
